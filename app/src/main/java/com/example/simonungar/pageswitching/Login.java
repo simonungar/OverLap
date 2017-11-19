4934a81d302d79fe4c28c5f696e1b66fda204a07
Simon Ungar <14seau@caslab.queensu.ca>
2017-11-19 13:03:54 -0500

    got rid of title bar, and the new start up page is Login.

--- app/src/main/java/com/example/simonungar/pageswitching/Login.java
+++ app/src/main/java/com/example/simonungar/pageswitching/Login.java
@@ -5,15 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        configureNextButton();
+//        configureNextButton();
     }
 
     private void configureNextButton() {
@@ -21,8 +27,9 @@ public class Login extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Login.this, SecondActivity.class));
+                startActivity(new Intent(Login.this, HomePage.class));
             }
         });
     }
+
 }
